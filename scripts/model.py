d2bdfc76dd0dc74e8095096a8fd5bd46fe2bf4da
Oliver Fei <ofei006@uottawa.ca>
2018-03-14 02:52:00 -0400

    add hidden layer to basic neural network. integrate parser for training, evaluation, and prediction

--- scripts/model.py
+++ scripts/model.py
@@ -1,19 +1,34 @@
 from keras.models import Model
 from keras.layers import Input, Dense
-from parser import Parser
+from util.parser import Parser
+from numpy import array
 import sys
 
-a = Input(shape=(21,))
-b = Dense(7)(a)
-model = Model(inputs=a, outputs=b)
-model.compile(optimizer='rmsprop',
+inputLayer = Input(shape=(21,))
+hiddenLayer = Dense(14)(inputLayer)
+outputLayer = Dense(7)(hiddenLayer)
+model = Model(inputs=inputLayer, outputs=outputLayer)
+model.compile(optimizer='adam',
               loss='mean_squared_error',
               metrics=['accuracy'])
 
 p = Parser()
-dataFile = sys.argv[1]
-p.runParse(dataFile)
-inputData = p.getInputs()
-outputData = p.getOutputs()
-model.fit(inputData, outputData)
+dataFileTrain = sys.argv[1]
+dataFileTest = sys.argv[2]
 
+inputDataTrain = array(p.Parse(dataFileTrain))
+print(inputDataTrain.shape)
+outputDataTrain = array(p.ParseSpine(dataFileTrain))
+print(outputDataTrain.shape)
+model.fit(inputDataTrain, outputDataTrain, 1)
+
+inputDataTest = array(p.Parse(dataFileTest))
+print(inputDataTest.shape)
+outputDataTest = array(p.ParseSpine(dataFileTest))
+print(outputDataTest.shape)
+loss_and_metrics = model.evaluate(inputDataTest, outputDataTest)
+print(loss_and_metrics)
+
+exampleValues = array([inputDataTest[0,:]])
+print(exampleValues)
+print(model.predict(exampleValues, 1))
