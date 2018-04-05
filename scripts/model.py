95ac29dd40e54cf826b9331b6d029d29bc8ffb84
Oliver Fei <ofei006@uottawa.ca>
2018-04-05 19:06:07 -0400

    wip

--- scripts/model.py
+++ scripts/model.py
@@ -8,6 +8,10 @@ import sys
 import json
 import matplotlib.pyplot as plt
 import keras.backend as K
+import keras
+import tensorflow as tf
+from tensorflow.python.tools import freeze_graph
+from tensorflow.python.saved_model import builder as saved_model_builder
 
 def weighted_mean_squared_error(y_true, y_pred):
     difference = y_pred - y_true
@@ -15,9 +19,8 @@ def weighted_mean_squared_error(y_true, y_pred):
     return K.mean(K.square((difference*weights)), axis=-1)
 
 inputLayer = Input(shape=(21,))
-hiddenLayer1 = Dense(20)(inputLayer)
-hiddenLayer2 = Dense(14)(hiddenLayer1)
-outputLayer = Dense(7)(hiddenLayer2)
+hiddenLayer1 = Dense(11)(inputLayer)
+outputLayer = Dense(7)(hiddenLayer1)
 model = Model(inputs=inputLayer, outputs=outputLayer)
 model.compile(optimizer='adam',
               loss='mean_squared_error',
@@ -31,7 +34,23 @@ inputDataTrain = array(p.Parse(dataFileTrain))
 print(inputDataTrain.shape)
 outputDataTrain = array(p.ParseSpine(dataFileTrain))
 print(outputDataTrain.shape)
-history = model.fit(inputDataTrain, outputDataTrain, 32, 2000)
+
+class TFCheckpointCallback(keras.callbacks.Callback):
+    def __init__(self, saver, sess):
+        self.saver = saver
+        self.sess = sess
+        self.count = 0
+
+    def on_epoch_end(self, epoch, logs=None):
+        self.count += 1
+        if self.count == 200:
+            self.saver.save(self.sess, 'freeze/checkpoint.ckpt', global_step=epoch)
+
+tf_graph = K.get_session().graph
+tf_saver = tf.train.Saver()
+tfckptcb = TFCheckpointCallback(tf_saver, K.get_session())
+
+history = model.fit(inputDataTrain, outputDataTrain, 32, 200, callbacks=[tfckptcb])
 
 print(history.history.keys())
 
@@ -81,3 +100,27 @@ if len(sys.argv) > 4:
     outputFile = open(sys.argv[4],'w')
     json.dump(array(estimate).tolist(), outputFile)
     outputFile.close()
+
+#export the model
+export_path = "../models"
+K.set_learning_phase(0)
+config = model.get_config()
+weights = model.get_weights()
+new_model = Model.from_config(config)
+new_model.set_weights(weights)
+ 
+tf.saved_model.simple_save( K.get_session(),
+                            export_path,
+                            inputs={'input': new_model.inputs[0]},
+                            outputs={'output': new_model.outputs[0]})
+
+freeze_graph.freeze_graph(input_graph = export_path +'/saved_model.pb',
+              input_binary = True,
+              input_checkpoint = './freeze/checkpoint.ckpt',
+              output_node_names = "action",
+              output_graph = export_path +'/saved_model.bytes' ,
+              clear_devices = True, initializer_nodes = "",input_saver = "",
+              restore_op_name = "save/restore_all", filename_tensor_name = "save/Const:0")
+
+ 
+
